11eb21d426ef4834217ea9400f3d8038f8cb640a
Floris van Breugel <florisvb@gmail.com>
2012-07-31 10:37:11 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='FlydraAnalysisTools',
+    version='0.0.1',
+    author='Floris van Breugel',
+    author_email='floris@caltech.edu',
+    packages = ['flydra_analysis_tools'],
+    license='BSD',
+    description='Analysis scripts for analyzing and plotting flydra data',
+    long_description=open('README.txt').read(),
+)
+
+
+
