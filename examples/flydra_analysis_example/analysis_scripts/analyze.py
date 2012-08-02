f7a92e8045c66a55496e40c3c147ad883c80a470
florisvb <florisvb@gmail.com>
2012-08-02 14:18:23 -0700

    added script for creating empty data directory

--- examples/flydra_analysis_example/analysis_scripts/analyze.py
+++ examples/flydra_analysis_example/analysis_scripts/analyze.py
@@ -2,8 +2,14 @@ import save_h5_to_dataset
 import prep_dataset
 import make_plots
 import analysis_configuration
+import generate_empy_data_directory
 
 config = analysis_configuration.Config()
+
+try:
+    generate_empty_data_directory
+except:
+    'Could not make empty data directory -- probably exists already!'
 
 if config.h5_files is not None:
     save_h5_to_dataset.main()
