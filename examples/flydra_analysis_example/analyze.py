f74a328c7cacb8dcb7cff1ae4e729e15db6d0c38
Floris van Breugel <caveman@flycave.(none)>
2012-08-01 19:59:57 -0700

    added support for skipping h5 analysis

--- examples/flydra_analysis_example/analyze.py
+++ examples/flydra_analysis_example/analyze.py
@@ -1,8 +1,13 @@
 import save_h5_to_dataset
 import prep_dataset
 import make_plots
+import analysis_configuration
 
-save_h5_to_dataset.main()
+config = analysis_configuration.Config()
+
+if config.h5_files is not None:
+    save_h5_to_dataset.main()
+
 prep_dataset.main()
 make_plots.main()
 
